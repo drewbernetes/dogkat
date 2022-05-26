c48efc5af7b1fe4822706f1ecd1a73baaee478ad
Drew <vilesy@gmail.com>
2022-05-26 15:06:53 +0100

    minor ingress query fix

--- test-cases/ingress.go
+++ test-cases/ingress.go
@@ -95,13 +95,13 @@ func testHostEndpoints(host string, counter int) error {
 	if err != nil {
 		if strings.Contains(err.Error(), strings.ToLower("no such host")) {
 			log.Printf("dns is not resolving for %s - retrying in %s seconds\n", host, delay)
-			testHostEndpoints(host, counter+1)
+			return testHostEndpoints(host, counter+1)
 		} else if strings.Contains(err.Error(), "x509: certificate") {
 			log.Printf("There is a certificate error for %s - Have you got a problem with cert-manager or external DNS? Retrying in %s seconds\n", host, delay)
-			testHostEndpoints(host, counter+1)
+			return testHostEndpoints(host, counter+1)
 		} else if strings.Contains(err.Error(), "No address associated with hostname") {
 			log.Printf("Address error for %s - retrying in %s seconds\n", host, delay)
-			testHostEndpoints(host, counter+1)
+			return testHostEndpoints(host, counter+1)
 		} else {
 			return err
 		}
